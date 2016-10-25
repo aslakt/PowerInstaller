002515bf51a299276a8bacae603895283b455080
Aslak Tangen <aslakt@gmail.com>
2016-10-25 23:04:00 +0200

    GetTreeViewItem()
    Restructured the code.
    Classes InstallSequence and SequenceItem were added to provide future
    GUI logic. These classes both implement GetTreeViewItem() - returning a
    System.Windows.Controls.TreeViewItem for use with WPF GUI.

--- bin/PowerInstaller.ps1
+++ bin/PowerInstaller.ps1
@@ -1,48 +1,24 @@
-﻿Class InstallSequence {
+﻿Class DetectionMethod {
 
-    [Installer[]]$Sequence
-    
-    InstallSequence(){
-        $this.Sequence = [System.Collections.ArrayList]::New()
-    }
+    [ValidateSet("File", "MSI", "Registry")][string]$DetectionType
+    [string]$Path
 
-    InstallSequence([Installer]$Sequence){
-        $this.Sequence = [System.Collections.ArrayList]$Sequence
-        $this.AddInstaller($Sequence)
-    }
-    
-    InstallSequence([Installer[]]$Sequence){
-        $this.Sequence = [System.Collections.ArrayList]$Sequence
+    DetectionMethod([string]$DetectionType, [string]$Path) {
+        $this.DetectionType = $DetectionType
+        $this.Path = $Path
     }
 
-    [int]AddInstaller([Installer]$Sequence) {
-        return ($this.Sequence.Add($Sequence))
+    [bool]Detected() {
+        if ($this.DetectionType -eq "File" -or $this.DetectionType -eq "Registry") {
+            return (Test-Path $this.Path)
+        } elseif ($this.DetectionType -eq "MSI") {
+            $Installer = New-Object -ComObject WindowsInstaller.Installer
+            return ($Installer.Productstate($this.Path) -eq 5)
+        } else {
+            return $false
+        }        
     }
 
-    [int]InsertInstaller([int]$index, [Installer]$Sequence) {
-        return ($this.Sequence.Insert($index, $Sequence))
-    }
-
-    [int]InsertInstallerSequence([int]$index, [Installer[]]$Sequence){
-        return ($this.Sequence.InsertRange($index, [System.Collections.ArrayList]$Sequence))
-    }
-
-    [int]RemoveInstaller([Installer]$Installer) {
-        return ($this.Sequence.Remove($Installer))
-    }
-
-    [int]RemoveInstaller([int]$index) {
-        return ($this.Sequence.RemoveAt($index))
-    }
-
-    [bool]ProcessSequence(){
-        ForEach ($installer in $this.Sequence) {
-            If (!($installer.Install())){
-                return $false
-            }
-        }
-        return $true
-    }
 }
 
 Class Installer {
@@ -50,11 +26,11 @@ Class Installer {
     [string]$Name
     [Version]$Version
     [DetectionMethod]$DetectionMethod
-    hidden [string]$Path
     [int[]]$ValidExitCodes
     [string]$FilePath
     [string[]]$InstallArgumentList
     [string[]]$UninstallArgumentList
+    hidden [string]$Path
 
     [bool]Install() {
         If (!($this.DetectionMethod.Detected())) {
@@ -78,14 +54,14 @@ Class Installer {
 
 Class SetupInstaller : Installer {
 
-    SetupInstaller([string]$Path, [DetectionMethod]$DetectionMethod){
+    SetupInstaller([string]$Path, [DetectionMethod]$DetectionMethod) {
         $this.Path = $Path
         $this.FilePath = $Path
         $this.DetectionMethod = $DetectionMethod
         $this.ValidExitCodes = @(0,3010)
     }
 
-    SetupInstaller([string]$Path, [DetectionMethod]$DetectionMethod, [string]$Name){
+    SetupInstaller([string]$Path, [DetectionMethod]$DetectionMethod, [string]$Name) {
         $this.Path = $Path
         $this.FilePath = $Path
         $this.DetectionMethod = $DetectionMethod
@@ -93,7 +69,7 @@ Class SetupInstaller : Installer {
         $this.ValidExitCodes = @(0,3010)
     }
 
-    SetupInstaller([string]$Path, [DetectionMethod]$DetectionMethod, [string]$Name, [Version]$Version){
+    SetupInstaller([string]$Path, [DetectionMethod]$DetectionMethod, [string]$Name, [Version]$Version) {
         $this.Path = $Path
         $this.FilePath = $Path
         $this.DetectionMethod = $DetectionMethod
@@ -150,41 +126,108 @@ Class MSIInstaller : Installer {
 
 }
 
-Class DetectionMethod {
-    [ValidateSet("File", "MSI", "Registry")][string]$DetectionType
-    [string]$Path
-    hidden [guid]$ProductCode
+Class SequenceItem {
 
-    DetectionMethod([string]$DetectionType, [string]$Path) {
-        $this.DetectionType = $DetectionType
-        $this.Path = $Path
-        If ($DetectionType -eq "MSI") {
+    [Installer[]]$Installers
+    [string]$Name
 
-            # Load some TypeData
-            Update-TypeData -AppendPath $PSScriptRoot\comObject.types.ps1xml
+    SequenceItem([Installer]$Installer) {
+        $this.Name = $Installer.Name
+        $NewInstallers = New-Object System.Collections.ArrayList
+        $NewInstallers.Add($Installer)
+        $this.Installers = $NewInstallers
+    }
 
-            $Installer = New-Object -ComObject WindowsInstaller.Installer
-            $InstallerDataBase = $Installer.InvokeMethod("OpenDatabase", (Resolve-Path $Path).Path, 0)
-            $View = $InstallerDataBase.InvokeMethod("OpenView", "SELECT `*` FROM `Property` ")
-            $View.InvokeMethod("Execute")
-            $Record = $View.InvokeMethod("Fetch")
-            $ProductProperties = [hashtable]::new()
-            While ($Record -ne $null) {
-                $ProductProperties.Add($Record.InvokeParamProperty("StringData", 1), $Record.InvokeParamProperty("StringData", 2))
-                $Record = $View.InvokeMethod("Fetch")
-            }
-            $this.ProductCode = [guid]$ProductProperties.Item("ProductCode")
+    SequenceItem([string]$Name, [Installer]$Installer) {
+        $this.Name = $Name
+        $NewInstallers = New-Object System.Collections.ArrayList
+        $NewInstallers.Add($Installer)
+        $this.Installers = $NewInstallers
+    }
+
+    SequenceItem([string]$Name, [Installer[]]$Installers) {
+        $this.Name = $Name
+        $this.Installers = $Installers
+    }
+
+    [int]AddInstaller([Installer]$Sequence) {
+        return ($this.Sequence.Add($Sequence))
+    }
+
+    [int]InsertInstaller([int]$index, [Installer]$Sequence) {
+        return ($this.Sequence.Insert($index, $Sequence))
+    }
+    
+    [int]RemoveInstaller([Installer]$Installer) {
+        return ($this.Sequence.Remove($Installer))
+    }
+
+    [int]RemoveInstaller([int]$index) {
+        return ($this.Sequence.RemoveAt($index))
+    }
+
+    [System.Windows.Controls.TreeViewItem]GetTreeViewItem([System.Windows.Controls.TreeViewItem]$Parent){
+        $TreeViewItem = [System.Windows.Controls.TreeViewItem]::new()
+        $TreeViewItem.Header = $this.Name
+        $TreeViewItem.Name = ("seq_" + ($this.Name -replace '[\W_]', ""))
+        $TreeViewItem.Tag = ($Parent.Tag + "\" + $this.Name)
+        ForEach ($Installer in $this.Installers) {
+            $ChildItem = [System.Windows.Controls.TreeViewItem]::new()
+            $ChildItem.Header = $Installer.Name
+            $ChildItem.Name = ("inst_" + ($Installer.Name -replace '[\W_]', ""))
+            $ChildItem.Tag = ($TreeViewItem.Tag + "\" + $Installer.Name)
+            $TreeViewItem.Items.Add($ChildItem)
         }
+        return $TreeViewItem
+    }
+}
+
+Class InstallSequence {
+
+    [SequenceItem[]]$Sequence
+    [string]$Name
+    
+    InstallSequence([string]$Name) {
+        $this.Name = $Name
+        $this.Sequence = [System.Collections.ArrayList]::New()
     }
 
-    [bool]Detected() {
-        if ($this.DetectionType -eq "File" -or $this.DetectionType -eq "Registry") {
-            return (Test-Path $this.Path)
-        } elseif ($this.DetectionType -eq "MSI") {
-            $Installer = New-Object -ComObject WindowsInstaller.Installer
-            return ($Installer.Productstate(("{" + $this.ProductCode + "}")) -eq 5)
-        } else {
-            return $false
-        }        
+    InstallSequence([string]$Name, [SequenceItem]$SequenceItem) {
+        $this.Name = $Name
+        $NewSequence = [System.Collections.ArrayList]::New()
+        $NewSequence.Add($SequenceItem)
+        $this.Sequence = $NewSequence
     }
+    
+    InstallSequence([string]$Name, [SequenceItem[]]$Sequence) {
+        $this.Name = $Name
+        $this.Sequence = $Sequence
+    }
+
+    [int]InsertInstallerSequence([int]$index, [SequenceItem[]]$Sequence){
+        return ($this.Sequence.InsertRange($index, [System.Collections.ArrayList]$Sequence))
+    }
+
+    [bool]ProcessSequence(){
+        ForEach ($installer in $this.Sequence) {
+            If (!($installer.Install())){
+                return $false
+            }
+        }
+        return $true
+    }
+
+    [System.Windows.Controls.TreeViewItem]GetTreeViewItem(){
+        $TreeViewItem = [System.Windows.Controls.TreeViewItem]::new()
+        $TreeViewItem.Header = $this.Name
+        $TreeViewItem.Name = ("instSeq_" + ($this.Name -replace '[\W_]', ""))
+        $TreeViewItem.Tag = $this.Name
+        ForEach ($Sequence in $this.Sequence) {
+            [System.Windows.Controls.TreeViewItem]$ChildItem = $Sequence.GetTreeViewItem($TreeViewItem)
+            $ChildItem.Tag = ($TreeViewItem.Tag + "\" + $ChildItem.Tag)
+            $TreeViewItem.Items.Add($ChildItem)
+        }
+        return $TreeViewItem
+    }
+
 }
